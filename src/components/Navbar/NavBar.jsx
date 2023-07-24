88eff30c0572c5091af1b7afd6810fb3dc9c9613
Alan Maciel <alan.ivan.maciel@gmail.com>
2023-07-24 16:01:48 -0300

    1.1 NavBar con menu Hamburguesa

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,14 +1,26 @@
+import React, { useState } from "react";
 import "./NavBar.css";
 
 function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <header>
-      <h1>miWiki...</h1>
-      <div className="Menu">
-        <button>Uno</button>
-        <button>Uno</button>
-        <button>Uno</button>
+    <header className="NavBar">
+      <h1 className="Logo">miWiki...</h1>
+      <div className={isOpen ? "Menu open" : "Menu close"} id="_Menu" >
+        <button>Botón</button>
+        <button>Botón</button>
+        <button>Botón</button>
       </div>
+      <button className="Toggle" id="_Toggle" onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </button>
     </header>
   );
 }
