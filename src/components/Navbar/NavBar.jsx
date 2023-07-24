f1acd94448ce31a7b22cf80205c77825434a90ca
Alan Maciel <alan.ivan.maciel@gmail.com>
2023-07-24 14:17:35 -0300

    1.0 Creando NavBar

---
+++ src/components/Navbar/NavBar.jsx
@@ -0,0 +1,16 @@
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <header>
+      <h1>miWiki...</h1>
+      <div className="Menu">
+        <button>Uno</button>
+        <button>Uno</button>
+        <button>Uno</button>
+      </div>
+    </header>
+  );
+}
+
+export default NavBar;
